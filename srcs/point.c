37fb4d1a663fd625f0162d1ecf57d06f0d5eea34
Lena GIACALONE <lgiacalo@student.42.fr>
2016-12-14 05:43:32 +0100

    best constante

--- srcs/point.c
+++ srcs/point.c
@@ -6,7 +6,7 @@
 /*   By: lgiacalo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/12 00:03:24 by lgiacalo          #+#    #+#             */
-/*   Updated: 2016/12/14 05:37:59 by lgiacalo         ###   ########.fr       */
+/*   Updated: 2016/12/14 05:43:03 by lgiacalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,8 +51,8 @@ t_point	ft_conv_point(t_env *env, int x, int y)
 	point.y = y + env->h_more + (ECT_PIX * y);
 	if (env->point[y][x].z != 0)
 	{
-		point.x = (int)(point.x - 2 * env->point[y][x].z);
-		point.y = (int)(point.y - (2.5) * env->point[y][x].z);
+		point.x = (int)(point.x - 1.5 * env->point[y][x].z);
+		point.y = (int)(point.y - (3) * env->point[y][x].z);
 	}
 	return (point);
 }
